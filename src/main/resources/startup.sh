1d9864e35de48e9b2d4ef4a8bef0d882c89cf53c
c99happy <1319805981@qq.com>
2021-07-14 10:31:17 +0800

    Add files via upload
    ff

--- src/main/resources/startup.sh
+++ src/main/resources/startup.sh
@@ -1,16 +1,31 @@
-# ----------------------------------------------------
-# Start script for the Socekt Server
-# ----------------------------------------------------
-#!/bin/sh
-ps -ef |grep demo.jar |grep -v grep
-if [ $? -eq 0 ];then
-  echo 'Socket server is running!'
+if [ -z "$JAVA_HOME" ]; then
+   error_exit "Please set the JAVA_HOME variable in your environment, We need java(x64)! jdk8 or later is better!"
+fi
+  export JAVA_HOME
+  export JRE_HOME=${JAVA_HOME}/jre
+  export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
+  export SERVER_NAME="demo"
+  export JAVA="$JAVA_HOME/bin/java"
+  export BASE_DIR=`cd $(dirname $0)/.; pwd`
+  export DEFAULT_SEARCH_LOCATIONS="classpath:/,classpath:/config/,file:./,file:./conf ig/"
+  export CUSTOM_SEARCH_LOCATIONS=${DEFAULT_SEARCH_LOCATIONS},file:${BASE_DIR}/conf/
+    JAVA_OPT="-XX:-OmitStackTraceInFastThrow -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${BASE_DIR}/logs/java_heapdump.hprof"
+    JAVA_OPT="${JAVA_OPT} -XX:-UseLargePages" JAVA_OPT="${JAVA_OPT} -jar ${BASE_DIR}/${SERVER_NAME}*.jar"
+    JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
+    JAVA_OPT="${JAVA_OPT} --spring.config.location=${CUSTOM_SEARCH_LOCATIONS}"
+if [ ! -d "${BASE_DIR}/logs" ]; then
+mkdir ${BASE_DIR}/logs
+fi
+echo "$JAVA ${JAVA_OPT}"
+if [ ! -f "${BASE_DIR}/logs/${SERVER_NAME}.out" ]; then
+touch "${BASE_DIR}/logs/${SERVER_NAME}.out"
+fi
+echo "$JAVA ${JAVA_OPT}" > ${BASE_DIR}/logs/${SERVER_NAME}.out 2>&1 &nohup $JAVA ${JAVA_OPT} jingnan_web.jingnan_web >> ${BASE_DIR}/logs/${SERVER_NAME}.out 2>&1 &
+echo "server is starting，you can check the ${BASE_DIR}/logs/${SERVER_NAME}.out"
+sleep 10
+pid=`ps ax | grep -i 'demo' | grep java | grep -v grep | awk '{print $1}'`
+if [ -z "$pid" ] ; then
+    echo "Start successfully!."
 else
-    netstat -apn |grep 1024
-    if [ $? -eq 0 ];then
-         PID_1024 = $(echo `netstat -apn |grep 1024 | awk '{print $NF}'|awk -F '/' '{print $1}'`)
-         kill  $PID_1024
-    fi
-    echo "startup socket server:"
-    nohup java -jar demo.jar &
+       echo "Start fail!."
 fi
--- src/main/resources/stop.sh
+++ src/main/resources/stop.sh
@@ -1,8 +1,7 @@
-jar_name=demo.jar
-echo "Stopping" ${jar_name}
-pid=`ps -ef | grep ${jar_name} | grep -v grep | awk '{print $2}'`
-if [ -n "$pid" ]
-then
-   echo "stop" ${jar_name} $pid
-   kill -9 $pid
+pid=`ps ax | grep -i 'demo' | grep java | grep -v grep | awk '{print $1}'`
+if [ -z "$pid" ] ; then
+    echo "No demoServer running."
+else
+    echo "The demoServer(${pid}) is running..." kill -9 ${pid}
+    echo "Send shutdown request to demoServer(${pid}) OK"
 fi
